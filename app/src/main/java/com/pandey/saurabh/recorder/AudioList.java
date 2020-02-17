f709e475a305cbf653ae79f0b0ccd0d2e0c70d26
Saurabh Pandey <saurabh17100@gmail.com>
2020-02-18 04:08:28 +0530

    Version 1.0 Music player hide problem resolved using bottom sheet behavior sheet method (5 3.30)

--- app/src/main/java/com/pandey/saurabh/recorder/AudioList.java
+++ app/src/main/java/com/pandey/saurabh/recorder/AudioList.java
@@ -3,17 +3,26 @@ package com.pandey.saurabh.recorder;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AudioList extends Fragment {
+
+    private RelativeLayout player_sheet;
+    private BottomSheetBehavior bottomSheetBehavior;
 
 
     public AudioList() {
@@ -28,4 +37,25 @@ public class AudioList extends Fragment {
         return inflater.inflate(R.layout.fragment_audio_list, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        player_sheet=view.findViewById(R.id.player_sheet);
+        bottomSheetBehavior=BottomSheetBehavior.from(player_sheet);
+
+        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if(newState == BottomSheetBehavior.STATE_HIDDEN){
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+    }
 }
